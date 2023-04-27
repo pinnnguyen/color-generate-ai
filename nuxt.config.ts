ab4f0156b074cfc307f7261e9d40bee5c1703ae2
pinnnguyen <pinnguyen0597@gmail.com>
2023-04-27 16:03:42 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,4 +12,24 @@ export default defineNuxtConfig({
     'nuxt-icon',
     '@vueuse/nuxt',
   ],
+  app: {
+    keepalive: true,
+    head: {
+      viewport: 'width=device-width,initial-scale=1,viewport-fit=cover',
+      bodyAttrs: {
+        class: 'overflow-x-hidden',
+      },
+      link: [
+        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
+      ],
+      title: 'AI Colors',
+      meta: [
+        {
+          name: 'copyright',
+          content: 'AI Colors',
+        },
+      ],
+    },
+  },
 })
